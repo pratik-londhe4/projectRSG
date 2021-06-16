6507c9061504bf29102e4db10c652a4d4210609e
Pratik <pratik.londhe@moderncoe.edu.in>
2021-06-16 21:43:20 +0530

    added linux shell script

--- RSG.py
+++ RSG.py
@@ -1,8 +1,8 @@
 import pandas
 import random
 df = pandas.read_csv('songs.csv')
-ch = random.randint(0,7)
-print(df)
+ch = random.randint(0,34)
+print("                                  "+df['name'][ch])
 
 
 
