c0f24e41fef39f11911fbe13e12732b709aa045e
pratik-londhe4 <pratik.londhe@moderncoe.edu.in>
2022-03-26 04:37:19 +0530

    update project

--- RSG.py
+++ RSG.py
@@ -1,8 +1,11 @@
 import pandas
 import random
-#from selenium import webdriver
-#from selenium.webdriver.common.keys import Keys
-#driver = webdriver.Firefox()
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait 
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+driver = webdriver.Firefox(executable_path='./geckodriver.exe')
 
 df = pandas.read_csv('songs.csv')
 ch = random.randint(0,34)
@@ -11,11 +14,11 @@ print("")
 print("                                         Link to the song!")
 #print("Opening Song in Browser")
 song = str(df['link'][ch])
-print("                                    "+"\033[2;36;40m"+song)
-print("\033[1;37;40m")
-#driver.get(song)
+print(song + "/")
 
-
+driver.get(song+"/?autoplay=1")
+element = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH , "/html/body/ytd-app/div[1]/ytd-page-manager/ytd-watch-flexy/div[5]/div[1]/div/div[1]/div/div/div/ytd-player/div/div/div[5]/button")))
+element.click()
 
 
 
